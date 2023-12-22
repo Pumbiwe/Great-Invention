a5b8180032b98316d3ce4c319aa531d8ea69a882
Pumbiwe <pumbiwedev@gmail.com>
2023-12-22 19:55:48 +0300

    Added buttons

--- main.py
+++ main.py
@@ -14,13 +14,23 @@ if __name__ == '__main__':
     size = width, height = 720, 500
     screen = pygame.display.set_mode(size)
     running = True
+    
+    screen.fill(VERY_DARK_BG)
+    hz = Text(screen, text="Here is the name of the game...", coordinates=(width // 2, height // 2))
+    new = Text(screen, text="Why am I doing it???", coordinates=(width // 2, height // 2 + 22), font_size=12, text_color=GREY)
+    
+    buttons = list()
+    btn = Button(
+        screen, text="Start", coordinates=(width - 100, height - 65, 100, 50)
+    )
+    buttons.append(btn)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-    
-        screen.fill(VERY_DARK_BG)
-        hz = Text(screen, text="Here is the name of the game...", coordinates=(width // 2, height // 2))
-        new = Text(screen, text="Why am I doing it???", coordinates=(width // 2, height // 2 + 22), font_size=12, text_color=GREY)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                for button in buttons:
+                    button.pressed(event.pos)
+        
         pygame.display.flip()
 
--- widgets.py
+++ widgets.py
@@ -26,3 +26,71 @@ class Text:
         self.screen.blit(self.text, self.text_rect)
         
 
+class Button:
+    def __init__(
+            self,
+            screen,
+            text="Default",
+            background_color=LIGHT,
+            coordinates=(0, 0),
+            border_color=GREY,
+            text_color=VERY_DARK_BG,
+            font_size=24,
+            on_clicked=None
+        ):
+        if not on_clicked:
+            on_clicked = self.on_clicked
+        self.on_clicked = on_clicked
+        self.text_color = text_color
+        self.border_color = border_color
+        self.coordinates = coordinates
+        self.screen = screen
+        self.text = text
+        self.font_size = font_size
+        self.background_color = background_color
+        self.is_hovered = False
+        self.update()
+    
+    def pressed(self, mouse_position):
+        if all([
+            mouse_position[0] >= self.coordinates[0] - self.coordinates[2] // 2,
+            mouse_position[0] <= self.coordinates[0] + self.coordinates[2] // 2,
+            mouse_position[1] >= self.coordinates[1] - self.coordinates[3] // 2,
+            mouse_position[1] <= self.coordinates[1] + self.coordinates[3] // 2
+        ]):
+            self.on_clicked()
+    
+    def on_clicked(self):
+        print('CLICKED')
+    
+    def update(self):
+        coord = list(self.coordinates)
+        coord[0] -= coord[2] // 2
+        coord[1] -= coord[3] // 2
+        pygame.draw.rect(
+            self.screen, 
+            self.background_color, 
+            pygame.Rect(coord), 100, 3
+        )
+        pygame.draw.rect(
+            self.screen, 
+            self.border_color if not self.is_hovered else self.hover_color,
+            pygame.Rect(coord), 2, 3
+        )
+        self.update_label()
+    
+    def update_label(self):
+        try:
+            del self.label
+        except:
+            ...
+        coord = list(self.coordinates[:2])
+        self.label = Text(
+            self.screen, 
+            text=self.text, 
+            coordinates=coord, 
+            text_color=self.text_color,
+            font_size=self.font_size
+        )
+        
+        del coord

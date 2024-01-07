ced5a67897e782abfd3efe7ecb61e8e5d8c88259
Pumbiwe <pumbiwedev@gmail.com>
2024-01-07 17:34:48 +0300

    Release

--- compile_project_to_exe.py
+++ compile_project_to_exe.py
@@ -2,17 +2,20 @@ import os
 import shutil
 
 
+shutil.rmtree('dist', ignore_errors=True)
+
 os.system(
-    "pyinstaller -i assets/chemistry.png --onefile --noconfirm --noconsole main.py"
+    "pyinstaller -i assets/chemistry.png --noconfirm --noconsole main.py"
 )
 print('Compiled.')
 for folder in {'assets', 'fonts'}:
     try:
         shutil.copytree(
-            os.path.join(os.getcwd(), folder), os.path.join(os.getcwd(), 'dist', folder)
+            os.path.join(os.getcwd(), folder), os.path.join(os.getcwd(), 'dist', 'main', folder)
         )
     except FileExistsError as e:
         ...
 
 for filename in {'database.db'}:
     shutil.copyfile(os.path.join(os.getcwd(), filename), os.path.join(os.getcwd(), 'dist', filename))
+shutil.rmtree('build', ignore_errors=True)
